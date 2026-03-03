0ec5842b82a68e431614aeb214c319ff50bd27c7
u1333020 <u1333020@o365.nuu.edu.tw>
2026-03-03 10:02:51 +0800

    work

--- index.php
+++ index.php
@@ -19,11 +19,11 @@ $randomMessage = $messages[array_rand($messages)];
 <html lang="zh-TW">
 <head>
     <meta charset="UTF-8">
-    <title>簡單 PHP 範例</title>
+    <title> PHP 範例</title>
 </head>
 <body>
     <h1>目前時間：<?= date('Y-m-d H:i:s') ?></h1>
     <p>隨機訊息：<?= htmlspecialchars($randomMessage) ?></p>
-    
+
 </body>
 </html>
