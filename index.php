f6041d1646793f9bc539789c60b5a2f71546b359
u1333020 <u1333020@o365.nuu.edu.tw>
2026-03-03 09:45:15 +0800

    homework

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
+// index.php
 
+// 設定時區
+date_default_timezone_set('Asia/Taipei');
 
+// 隨機訊息陣列
+$messages = [
+    'Hello, world!',
+    '今天是個好日子。',
+    '隨機數字：' . rand(1, 100),
+    'PHP 程式碼範例',
+];
+
+// 從陣列中挑一個訊息
+$randomMessage = $messages[array_rand($messages)];
 ?>
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <title>簡單 PHP 範例</title>
+</head>
+<body>
+    <h1>目前時間：<?= date('Y-m-d H:i:s') ?></h1>
+    <p>隨機訊息：<?= htmlspecialchars($randomMessage) ?></p>
+</body>
+</html>
